35a5dee279bb50b3d3ef4c59ff46974f45a00a5a
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2022-07-27 17:32:30 -0700

    Sample chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { data as sample } from "./data";
+
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  LineChart 
+} from "recharts";
+
+interface CoffeeData {
+  t: number | undefined;
+  pressure: number | undefined;
+  weight: number | undefined;
+  flow: number | undefined;
+  pwm: number | undefined;
+  xProfile: number | undefined;
+  yProfile: number | undefined;
+}
+
+const initialData: CoffeeData[] = sample.map(
+  ([t, pressure, weight, flow, pwm, xProfile, yProfile]) => ({
+    t,
+    pressure,
+    weight,
+    flow,
+    pwm,
+    xProfile,
+    yProfile,
+  })
+);
 
 function App() {
+  const [data, setData] = useState<CoffeeData[]>([]);
+  useEffect(() => {
+    setInterval(() => {
+      if (initialData.length) {
+        const d = initialData.shift()
+        if (d){
+          console.log(d)
+          setData((x) => [...x, d]);
+        }
+      }
+    }, 100);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ width: "100%", height: 300 }}>
+      <ResponsiveContainer>
+        <AreaChart
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="t" height={0}/>
+          <YAxis />
+          <Tooltip />
+          <Area isAnimationActive={false} type="monotone" dataKey="pressure" stroke="#8884d8" fillOpacity={0} />
+          <Area isAnimationActive={false} type="monotone" dataKey="flow" stroke="#000"  fillOpacity={0}/>
+          <Area isAnimationActive={false} type="monotone" dataKey="weight" stroke="#34e5eb" fillOpacity={0}/>
+          <Area isAnimationActive={false} type="monotone" dataKey="pwm" stroke="#b3258d"  fillOpacity={0}/>
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   );
 }
